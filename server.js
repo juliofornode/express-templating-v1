e085b2e10a7b26d34c5a17b2fca0e2161443bdc7
Julio <juliofornode@gmail.com>
2016-04-02 10:50:41 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+//require modules
+
+//instantiate express()
+
+//use and configure bodyParser
+
+//connect database, configure schema, set model
+
+//instantiate express.Router()
+
+//set routes
+
+//set routes prefix
+
+//set server port
+
